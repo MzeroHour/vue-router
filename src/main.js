6c867c26147c6e61bb263e7af40ea86bb57a92b3
mzero <myintzaw2010@gmail.com>
2023-04-28 16:12:10 +0630

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Import all of Bootstrap's CSS
+// import "bootstrap/scss/bootstrap";
+import "popper.js/dist/popper.min.js";
+import jQuery from "jquery";
+window.jQuery = window.$ = jQuery;
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
